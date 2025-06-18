0eae10a10edc11a8498e00515d71c0dd6f484099
c0ding-quest <104649234+c0ding-quest@users.noreply.github.com>
2025-06-18 11:35:28 +0200

    124

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,10 +8,7 @@ def calculator():
         result = num1 + num2
     elif op == '-':
         result = num1 - num2
-    elif op == '*':
-        result = num1 * num2
-    elif op == '/':
-        if num2 == 0:
+
             print("Error: Division by zero")
             return
         result = num1 / num2
